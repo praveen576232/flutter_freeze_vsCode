c4236c9cfa54e956771ad0a12385116c079244e5
praveen <48440981+praveen576232@users.noreply.github.com>
2022-07-03 20:00:19 +0530

    Update freezed.js

--- Freezed/freezed.js
+++ Freezed/freezed.js
@@ -1,16 +1,29 @@
 const fs = require("fs")
 const path = require("path")
-const generateClass = require("./class/generateClass") 
-function removeQuestion(str){
-    if(str.endsWith("?")){
-        return str.substring(0,str.length-1)
-    }
-    return str;
-}
-  
+const generateClass = require("./class/generateClass")
+
+
 function generateFile(object) {
-    if (object == null) return null
-    fs.writeFileSync(path.join(__dirname,"t.dart"),generateClass(object))
+    if (object == null) 
+        return null
+    
+    let folders;
+    console.log( object.filename);
+    let filename = object.filename.match(/(\w+.g.dart)/g)
+    console.log(filename);
+    if (filename != null && filename.length > 0) {
+        folders = object.filename.replace(filename[0], "")
+        console.log(folders);
+        if (folders != null && folders != "") {
+        let folderpath = folders.match(/((..\/|\/)\w+)*/g)
+        if (folderpath != null && folderpath.length > 0) {
+            console.log(folderpath[0]);
+            fs.mkdirSync(path.join(__dirname, folderpath[0]), {recursive: true})
+        }
+    }
+    }
+    
+    fs.writeFileSync(path.join(__dirname, object.filename), generateClass(object))
 }
 
 
