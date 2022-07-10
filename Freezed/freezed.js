2710bb85142fd3e6b3b93f44150c69c83ae418c6
praveen <48440981+praveen576232@users.noreply.github.com>
2022-07-10 06:23:05 -0700

    Update freezed.js

--- Freezed/freezed.js
+++ Freezed/freezed.js
@@ -1,29 +1,33 @@
 const fs = require("fs")
 const path = require("path")
 const generateClass = require("./class/generateClass")
-
+const vscode = require('vscode');
 
 function generateFile(object) {
-    if (object == null) 
-        return null
-    
-    let folders;
-    console.log( object.filename);
-    let filename = object.filename.match(/(\w+.g.dart)/g)
-    console.log(filename);
-    if (filename != null && filename.length > 0) {
-        folders = object.filename.replace(filename[0], "")
-        console.log(folders);
-        if (folders != null && folders != "") {
-        let folderpath = folders.match(/((..\/|\/)\w+)*/g)
-        if (folderpath != null && folderpath.length > 0) {
-            console.log(folderpath[0]);
-            fs.mkdirSync(path.join(__dirname, folderpath[0]), {recursive: true})
+    return new Promise((resolve, reject) => {
+        if (object == null)
+            return reject("Something Went Wrong Hear Please Check The Docs What you miss!.")
+
+        let folders;
+
+        let filename = object.filename.match(/(\w+.g.dart)/g)
+   
+        if (filename != null && filename.length > 0) {
+            folders = object.filename.replace(filename[0], "")
+
+            if (folders != null && folders != "") {
+                fs.mkdirSync(path.join(vscode.window.activeTextEditor.document.uri.fsPath, `../${folders}`), { recursive: true })
+            }
         }
-    }
-    }
-    
-    fs.writeFileSync(path.join(__dirname, object.filename), generateClass(object))
+
+       try {
+        fs.writeFileSync(path.join(vscode.window.activeTextEditor.document.uri.fsPath, `../${object.filename}`), generateClass(object))
+        resolve("Create File!")
+       } catch (error) {
+        reject("Something Goes Wrong!, Please Check the Docs or File Again what you miss.")
+       }
+    })
+
 }
 
 
