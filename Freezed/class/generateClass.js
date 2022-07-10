9b102f2192960e23452c0602d8fabe1f30dd809a
praveen <48440981+praveen576232@users.noreply.github.com>
2022-07-10 06:23:47 -0700

    Update generateClass.js

--- Freezed/class/generateClass.js
+++ Freezed/class/generateClass.js
@@ -1,11 +1,13 @@
+const vscode = require("vscode")
 module.exports = function generateClass(classInfo) {
+    let depth = getDepthOfFolder(classInfo.filename)
   return `// coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target 
+part of '${generateDepthFolder(depth)}${getFileNameOfActiveTab(vscode.window.activeTextEditor.document.fileName)}';
 
-${classInfo.imports.join("\n")}
-${isDeepEqualImportRequire(classInfo.class)}
+
 ${classInfo.class.map((myClass)=>{
     return   `
 class ${myClass.className} {
@@ -32,7 +34,7 @@ Map<String,Object?> toJson(){
 
 static ${myClass.className} fromJson(Map<String , Object?> json){
     return ${myClass.className}(
-            ${myClass.parameters.map((parameter)=>`${parameter.name}:${parameter.inbuilt ? `json['${parameter.name}'] as ${removeQuestion(parameter.dataType)}` : fromJsonForClass(parameter)}`).join(",\n")}
+            ${myClass.parameters.map((parameter)=>`${parameter.name}:${parameter.inbuilt ? isOptionalDataType(parameter.dataType)?`json['${parameter.name}'] == null ? null : json['${parameter.name}'] as ${removeQuestion(parameter.dataType)}` : `json['${parameter.name}'] as ${removeQuestion(parameter.dataType)}` : `${fromJsonForClass(parameter)}` }`).join(",\n")}
     );
 }
 
@@ -79,34 +81,64 @@ function removeQuestion(str){
 function toJsonForClass(parameter) {
    if(parameter.dataType.startsWith("List")){
       return `${parameter.name}.map<Map<String,dynamic>>((data)=> data.toJson()).toList()` 
-   }else if(parameter.startsWith("Map")){
+   }else if(parameter.dataType.startsWith("Map")){
     return `${parameter.name}['${parameter.name}'].toJson()`
    }
    return `${parameter.name}.toJson()`
 }
 function fromJsonForClass(parameter) {
    if(parameter.dataType.startsWith("List")){
-      return `json['${parameter.name}'].map<${parameter.className}>((data)=> ${parameter.className}.fromJson(data)).toList()` 
-   }else if(parameter.startsWith("Map")){
-    return `${parameter.name}['${parameter.name}']`
+      return isOptionalDataType(parameter.dataType)?  `json['${parameter.name}'] == null ? null : json['${parameter.name}'].map<${parameter.className}>((data)=> (${parameter.className} as List).fromJson(data  as Map<String,Object?>)).toList()`  : `(json['${parameter.name}'] as List).map<${parameter.className}>((data)=> ${parameter.className}.fromJson(data as Map<String,Object?>)).toList()` 
+   }else if(parameter.dataType.startsWith("Map")){
+    return isOptionalDataType(parameter.dataType)?  `${parameter.name}['${parameter.name}'] == null ? null : ${parameter.name}['${parameter.name}']` : `${parameter.name}['${parameter.name}']`
    }
-   return `${parameter.className}.fromJson(json['${parameter.name}'])`
+   return isOptionalDataType(parameter.dataType)? `json['${parameter.name}'] == null ? null : ${parameter.className}.fromJson(json['${parameter.name}'])` :`${parameter.className}.fromJson(json['${parameter.name}']  as Map<String,Object?>)`
 }
-function isDeepEqualImportRequire(myClass){
-  if(myClass == null || myClass.length <= 0) return
-  let importStatement;
-  for (let index = 0; index < myClass.length; index++) {
-    if(myClass[index].parameters != null || myClass[index].parameters.length > 0 ){
-        importStatement =   myClass[index].parameters.map((parameter)=>{
-            if(!parameter.inbuilt && parameter.dataType.startsWith("List")){
-                return "import 'package:collection/collection.dart';"
-            }
-          }).join("")
-          if(importStatement != null && importStatement!="") return importStatement
-    } 
+// function isDeepEqualImportRequire(myClass){
+//   if(myClass == null || myClass.length <= 0) return
+//   let importStatement;
+//   for (let index = 0; index < myClass.length; index++) {
+//     if(myClass[index].parameters != null || myClass[index].parameters.length > 0 ){
+//         importStatement =   myClass[index].parameters.map((parameter)=>{
+//             if(!parameter.inbuilt && parameter.dataType.startsWith("List")){
+//                 return "import 'package:collection/collection.dart';"
+//             }
+//           }).join("")
+//           if(importStatement != null && importStatement!="") return importStatement
+//     } 
 
     
     
+// }
+// return (importStatement != null && importStatement!="") ?  importStatement :""
+// }
+
+
+function getDepthOfFolder(folderPath){
+  if(folderPath == null) return 0;
+  let folders  = folderPath.split("/")
+  if(folders.length > 2){
+    return folders.length - 2
+  }
+  return 0
 }
-return (importStatement != null && importStatement!="") ?  importStatement :""
+function generateDepthFolder(depth) {
+    if(depth == null) return ''
+    let depthString = ""
+    for (let index = 0; index < depth; index++) {
+        depthString+="../"
+        
+    }
+    return depthString
+}
+function getFileNameOfActiveTab(path) {
+    if(path == null) return null;
+    let fileNames = path.match(/(\w+\.dart)\s*/g)
+    if(fileNames !=null && fileNames.length > 0){
+        return fileNames[0]
+    }
+    return null
+}
+function isOptionalDataType(dataType) {
+    return dataType.endsWith("?")
 }
